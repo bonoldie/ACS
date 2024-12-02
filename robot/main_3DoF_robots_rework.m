f08cd0e01a065c825d6e6ee21c4cac38c2b425e8
Bonoldie <erribono01@gmail.com>
2024-12-02 13:17:04 +0100

    Reverse Newton-Euler fix

--- robot/main_3DoF_robots_rework.m
+++ robot/main_3DoF_robots_rework.m
@@ -54,9 +54,15 @@ CoM = {
 
 m = [0; 1; 1; 1;0];
 
-I_Link1 = parallelAxis(inertiaVec2tensor(cylinderInertia(m(1),0.02, 0, delta_0_1)), m(1), [-delta_0_1/2;0;0]);
-I_Link2 = parallelAxis(inertiaVec2tensor(prismInertia(m(2),0.3, 0.03, delta_1_2)), m(2), [0;0;-delta_1_2/2]);
-I_Link3 = parallelAxis(inertiaVec2tensor(cylinderInertia(m(3),0.02, 0, delta_2_3)), m(3), [-delta_2_3/2;0;0]);
+I_Link1_CoM = inertiaVec2tensor(cylinderInertia(m(1),0.02, 0, delta_0_1));
+I_Link2_CoM = inertiaVec2tensor(prismInertia(m(2),0.3, 0.03, delta_1_2));
+I_Link3_CoM = inertiaVec2tensor(cylinderInertia(m(3),0.02, 0, delta_2_3));
+
+MoI_CoM = {zeros(3), I_Link1_CoM, I_Link2_CoM, I_Link3_CoM, zeros(3)};
+
+I_Link1 = parallelAxis(I_Link1_CoM, m(1), [-delta_0_1/2;0;0]);
+I_Link2 = parallelAxis(I_Link2_CoM, m(2), [0;0;-delta_1_2/2]);
+I_Link3 = parallelAxis(I_Link3_CoM, m(3), [-delta_2_3/2;0;0]);
 
 MoI = {zeros(3), I_Link1, I_Link2, I_Link3, zeros(3)};
 
@@ -64,6 +70,8 @@ g =  [0;0;-9.81];
 
 % Custom robotic structure
 myRobot = loadRobot(transforms, framesName, jointsType, linksName, m, MoI, CoM, [q dq ddq], g);
+
+myRobot_I_CoM = loadRobot(transforms, framesName, jointsType, linksName, m, MoI_CoM, CoM, [q dq ddq], g);
 
 % myRobot.motors = {loadMotor}
 
@@ -86,16 +94,23 @@ robot.Gravity = g;
 
 zeroTorqueConfiguration = [
 %   q      dq    ddq
-    pi/2,  0,    0; % 1
-    0,     0,    0; % 2
-    0,     0,    0; % 3
+    pi/2,  0,    0;   % 1
+    0,     0,    0;   % 2
+    0,     0,    0;   % 3
+];
+
+PoseAConfiguration = [
+%   q      dq    ddq
+    pi/4,  0,    0;   % 1
+    -0.17,     0,    0;   % 2
+    -pi/9,     0,    0;   % 3
 ];
 
 velConfiguration = zeroTorqueConfiguration + [
 %   q    dq      ddq
     0,   -10.5,   0;... % 1
     0,   10.5,    0;... % 2
-    0,   5.5,    0;... % 3
+    0,   5.5,    0;...  % 3
 ];
 
 accelConfiguration = zeroTorqueConfiguration + [
@@ -104,6 +119,21 @@ accelConfiguration = zeroTorqueConfiguration + [
     0,   0       -10.5; % 2
     0,   0       -1.5; % 3
 ];
+
+velAConfiguration = PoseAConfiguration + [
+%   q    dq      ddq
+    0,   -10.5,   0;... % 1
+    0,   10.5,    0;... % 2
+    0,   5.5,    0;...  % 3
+];
+
+accelAConfiguration = PoseAConfiguration + [
+%   q    dq      ddq
+    0,   0       10.5;  % 1
+    0,   0       -10.5; % 2
+    0,   0       -1.5; % 3
+];
+
 
 if 0
     figure();
@@ -116,10 +146,18 @@ end
 [myEvaluatedRobot2, l_torques2] = evalRobot(myRobot, velConfiguration);
 [myEvaluatedRobot3, l_torques3] = evalRobot(myRobot, accelConfiguration);
 
+[myEvaluatedRobot1_A, l_torques1_A] = evalRobot(myRobot, PoseAConfiguration);
+[myEvaluatedRobot2_A, l_torques2_A] = evalRobot(myRobot, velAConfiguration);
+[myEvaluatedRobot3_A, l_torques3_A] = evalRobot(myRobot, accelAConfiguration);
+
 % Robotics toolbox inverse dynamics
 toolbox_toques1 = inverseDynamics(robot, zeroTorqueConfiguration(:, 1),zeroTorqueConfiguration(:, 2),zeroTorqueConfiguration(:, 3));
 toolbox_toques2 = inverseDynamics(robot, velConfiguration(:, 1),velConfiguration(:, 2),velConfiguration(:, 3));
 toolbox_toques3 = inverseDynamics(robot, accelConfiguration(:, 1),accelConfiguration(:, 2),accelConfiguration(:, 3));
+
+toolbox_toques1_A = inverseDynamics(robot, PoseAConfiguration(:, 1),PoseAConfiguration(:, 2),PoseAConfiguration(:, 3));
+toolbox_toques2_A = inverseDynamics(robot, velAConfiguration(:, 1),velAConfiguration(:, 2),velAConfiguration(:, 3));
+toolbox_toques3_A = inverseDynamics(robot, accelAConfiguration(:, 1),accelAConfiguration(:, 2),accelAConfiguration(:, 3));
 
 % Newton-Euler recursive method for inverse dynamics
 
@@ -137,12 +175,58 @@ motor3 = loadMotor(k3, Im3, [0;0;1]);
 friction3 = loadFriction(Fv3, Fs3);
 
 myRobot.motors = {motor1, motor2, motor3};
+myRobot_I_CoM.motors = {motor1, motor2, motor3};
 myRobot.frictions = {friction1, friction2, friction3};
+myRobot_I_CoM.frictions = {friction1, friction2, friction3};
 
-[ne_tau1] = newtonEuler(myRobot, he, [0;0;0], [0;0;0], [0;0;0]);
+[ne_tau1, r, r_c, ddp_c] = newtonEuler(myRobot_I_CoM, he, [0;0;0], [0;0;0], [0;0;0]);
 
 ne_torques1 = subs(ne_tau1,[q;dq;ddq],zeroTorqueConfiguration(:));
 ne_torques2 = subs(ne_tau1,[q;dq;ddq],velConfiguration(:));
 ne_torques3 = subs(ne_tau1,[q;dq;ddq],accelConfiguration(:));
 
+ne_torques1_A = subs(ne_tau1,[q;dq;ddq],PoseAConfiguration(:));
+ne_torques2_A = subs(ne_tau1,[q;dq;ddq],velAConfiguration(:));
+ne_torques3_A = subs(ne_tau1,[q;dq;ddq],accelAConfiguration(:));
+
+
+figure();
+
+jointsIndex = find(cell2mat(cellfun(@(x) ismember(x, {'Prismatic','Revolute'}), myRobot.jointsType, 'UniformOutput', 0)));
+
+for i=1:myRobot.DOF
+    R_b_i = myRobot.T_b_i{jointsIndex(i)}(1:3, 1:3);
+
+    com = subs(CoM{jointsIndex(i)}, [q], PoseAConfiguration(1:3, 1));
+    eval_ddp_c = subs(R_b_i * ddp_c{i}, [q;dq;ddq], PoseAConfiguration(:));
+
+    plot3(com(1), com(2), com(3), "*");
+
+    quiver3(com(1), com(2), com(3),eval_ddp_c(1), eval_ddp_c(2), eval_ddp_c(3), 1);
+    hold on;
+end
+
+plotframe();
+axis equal;
+axis auto;
+
+
+% figure();
+% 
+% jointsIndex = find(cell2mat(cellfun(@(x) ismember(x, {'Prismatic','Revolute'}), myRobot.jointsType, 'UniformOutput', 0)));
+% 
+% for i=1:myRobot.DOF
+%     trans_b_i = double(subs(myRobot.T_b_i{jointsIndex(i)}(1:3, 4), q, zeroTorqueConfiguration(1:3, 1)));
+%     R_b_i = myRobot.T_b_i{jointsIndex(i)}(1:3, 1:3);
+% 
+%     r_vec = double(subs(R_b_i*r{i}, q, zeroTorqueConfiguration(1:3, 1)));
+%     r_c_vec = double(subs(R_b_i*r_c{i}, q, zeroTorqueConfiguration(1:3, 1)));
+% 
+%     quiver3(trans_b_i(1), trans_b_i(2), trans_b_i(3), r_vec(1), r_vec(2), r_vec(3), 1);
+%     hold on;
+%     quiver3(trans_b_i(1), trans_b_i(2), trans_b_i(3), r_c_vec(1), r_c_vec(2), r_c_vec(3),1);
+% end
+% 
+% axis equal;
+% axis auto;
 
--- robot/rework/newtonEuler.m
+++ robot/rework/newtonEuler.m
@@ -1,4 +1,4 @@
-function [torques] = newtonEuler(roboticStructure, he, w0, dw0, ddp0)
+function [torques, r, r_c, ddp_c] = newtonEuler(roboticStructure, he, w0, dw0, ddp0)
     %NEWTONEULER Summary of this function goes here
     %   Detailed explanation goes here
 
@@ -11,17 +11,26 @@ function [torques] = newtonEuler(roboticStructure, he, w0, dw0, ddp0)
     dw_m = cell(1,3);
     ddp = cell(1,3);
     ddp_c = cell(1,3);
-    
+
+    ddp0 = ddp0 - [0;-9.81;0];
+
+    r = cell(1,3);
+    r_c = cell(1,3);
+
     % forward
     for i=1:roboticStructure.DOF
 
         R = roboticStructure.T{jointsIndex(i)}(1:3, 1:3);
+        R_b_i = roboticStructure.T_b_i{jointsIndex(i)}(1:3, 1:3);
         
+        r{i} = R' * roboticStructure.T{jointsIndex(i)}(1:3, 4);
+        r_c{i} = R_b_i' * (roboticStructure.CoM{jointsIndex(i)} - roboticStructure.T_b_i{jointsIndex(i)}(1:3,4));
+
         if i == 1
             w_i_minus_one = w0;
             dw_i_minus_one = dw0;
             ddp_i_minus_one = ddp0;
-        else 
+        else
             w_i_minus_one = w(i-1);
             dw_i_minus_one = dw(i-1);
             ddp_i_minus_one = ddp(i-1);
@@ -36,22 +45,18 @@ function [torques] = newtonEuler(roboticStructure, he, w0, dw0, ddp0)
         dw{i} = R'*dw_i_minus_one;
 
         if strcmp(roboticStructure.jointsType{jointsIndex(i)}, 'Revolute')
-            dw{i} = dw{i} + R' * (roboticStructure.jointsSymbol(i, 3) * z0 + cross(roboticStructure.jointsSymbol(i, 2)*w_i_minus_one, z0));
+            dw{i} = dw{i} + R' * ((roboticStructure.jointsSymbol(i, 3) * z0) + cross(roboticStructure.jointsSymbol(i, 2)*w_i_minus_one, z0));
         end
-        
-        r = roboticStructure.T_b_i{jointsIndex(i)}(1:3,4) - roboticStructure.T_b_i{jointsIndex(i)-1}(1:3,4);
-        r = roboticStructure.T_b_i{jointsIndex(i)}(1:3, 1:3) * r;
 
-        ddp{i} = R'*ddp_i_minus_one + cross(dw{i},r) + cross(w{i}, cross(w{i}, r));
+        
+        ddp{i} = R' * ddp_i_minus_one + cross(dw{i},r{i}) + cross(w{i}, cross(w{i}, r{i}));
 
         if strcmp(roboticStructure.jointsType{jointsIndex(i)}, 'Prismatic')
             ddp{i} = ddp{i} + R' * roboticStructure.jointsSymbol(i, 3) * z0 + cross(2 * roboticStructure.jointsSymbol(i, 3) * w{i}, R*z0);
         end
 
-        r_c = roboticStructure.CoM{jointsIndex(i)} - roboticStructure.T_b_i{jointsIndex(i)}(1:3,4);
-        r_c = roboticStructure.T_b_i{jointsIndex(i)}(1:3, 1:3) * r_c;
-            
-        ddp_c{i} = ddp{i}  + cross(dw{i}, r_c) + cross(w{i}, cross(w{i}, r_c));
+       
+        ddp_c{i} = ddp{i}  + cross(dw{i}, r_c{i}) + cross(w{i}, cross(w{i}, r_c{i}));
         
         dw_m{i} = dw_i_minus_one + ...
             roboticStructure.motors{i}.k *  roboticStructure.jointsSymbol(i, 3) * roboticStructure.motors{i}.rotationAxis + ...
@@ -65,27 +70,24 @@ function [torques] = newtonEuler(roboticStructure, he, w0, dw0, ddp0)
 
     % backword
     for i=roboticStructure.DOF:-1:1
-        R_plus = roboticStructure.T{jointsIndex(i)+1}(1:3, 1:3);
-        R_minus =  roboticStructure.T{jointsIndex(i)}(1:3, 1:3);
-        
-        r = roboticStructure.T_b_i{jointsIndex(i)}(1:3,4) - roboticStructure.T_b_i{jointsIndex(i)-1}(1:3,4);
-        r = roboticStructure.T_b_i{jointsIndex(i)}(1:3, 1:3) * r;
 
-        r_c = roboticStructure.CoM{jointsIndex(i)} - roboticStructure.T_b_i{jointsIndex(i)}(1:3,4);
-        r_c = roboticStructure.T_b_i{jointsIndex(i)}(1:3, 1:3) * r_c;
-            
+        R_b_i = roboticStructure.T_b_i{jointsIndex(i)}(1:3, 1:3);
+        R_plus = roboticStructure.T{jointsIndex(i)+1}(1:3, 1:3);
+        R_minus = roboticStructure.T{jointsIndex(i)}(1:3, 1:3);
+        
+        % r = R' * roboticStructure.T{jointsIndex(i)}(1:3, 4);
+        % r_c = R_b_i' * (roboticStructure.CoM{jointsIndex(i)} - roboticStructure.T_b_i{jointsIndex(i)}(1:3,4));
 
         if i == roboticStructure.DOF
             f_i_plus_one = he(1:3);
-            mu_u_plus_one = he(4:6);
+            mu_i_plus_one = he(4:6);
         else
             f_i_plus_one = f{i+1};
-            mu_u_plus_one = mu{i+1};
+            mu_i_plus_one = mu{i+1};
         end
-
         
         f{i} = R_plus * f_i_plus_one + roboticStructure.mass(jointsIndex(i)) * ddp_c{i};
-        mu{i} = cross(- f{i}, (r + r_c)) +  R_plus * mu_u_plus_one + cross(R_plus * f_i_plus_one, r_c) + roboticStructure.MoI{jointsIndex(i)} * dw{i} + ...
+        mu{i} = - cross(f{i}, (r{i} + r_c{i})) +  R_plus * mu_i_plus_one + cross(R_plus * f_i_plus_one, r_c{i}) + roboticStructure.MoI{jointsIndex(i)} * dw{i} + ...
             + cross(w{i}, roboticStructure.MoI{jointsIndex(i)} * w{i});
         
         % TODO: add motor components
@@ -96,5 +98,4 @@ function [torques] = newtonEuler(roboticStructure, he, w0, dw0, ddp0)
             torques(i) = f{i}'*R_minus'*z0 + roboticStructure.frictions{i}.Fv * roboticStructure.jointsSymbol(i, 2) + roboticStructure.frictions{i}.Fs * sign(roboticStructure.jointsSymbol(i, 2));
         end
     end
-    
 end
